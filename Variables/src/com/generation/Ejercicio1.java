aab075b10e20bd12406bd37a3ece695c58c7b967
LuisMejiasR <luisme.1993@gmail.com>
2022-05-23 15:29:37 -0400

    area de un triangulo

---
+++ Variables/src/com/generation/Ejercicio1.java
@@ -0,0 +1,40 @@
+package com.generation;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //calculo area y calculo del perimetro
+        //crear variables que permitan a futuro pasarle de manera dinamica valores
+        //variables estaticas:
+
+
+        //formula area triangulo a = (altura * base) / 2;
+        long alturaTri = 4;
+        long baseTri = 5;
+        int dos = 2;
+        long radioCir = 4;
+        long diametroCir = 5;
+        float PI = 3.14f;
+
+        //impresion
+        System.out.println("************************");
+        System.out.println("****CALCULO DEL AREA****");
+        System.out.println("************************");
+        //formula
+
+        System.out.println("El area de un triangulo es " + ((alturaTri * baseTri)/ dos));
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+        //formula perimetro circulo
+        //impresion
+        System.out.println("************************");
+        System.out.println("**CALCULO DE PERIMETRO**");
+        System.out.println("************************");
+
+        System.out.println("El perimetro de un circulo es " + (" "));
+
+
+    }
+}
