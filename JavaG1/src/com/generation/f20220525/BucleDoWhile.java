361fe381cecd6db9bb4e1a9c2cbfe984ad76baba
LuisMejiasR <luisme.1993@gmail.com>
2022-05-25 18:08:39 -0400

    DoWhile y Ejercicio grupal n2

--- JavaG1/src/com/generation/f20220525/BucleDoWhile.java
+++ JavaG1/src/com/generation/f20220525/BucleDoWhile.java
@@ -1,7 +1,31 @@
 package com.generation.f20220525;
 
+import java.util.Scanner;
+
 public class BucleDoWhile {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        /*
+        // DoWhile
+        // a lo menos se ejecutara una vez
+        int termino = 8;
+        while (termino <= 10) {
+            System.out.println("While: el valor de termino es " + termino);
+            termino++;
+        }
+        do {
+
+            System.out.println("DoWhile: el valor de termino es " + termino);
+                termino++;
+        } while (termino <= 10);*/
+
+        System.out.println("Ingrese:\n (0) salir \n (1) sumar \n (2) restar \n (3) multiplicar \n (4) dividir");
+
+        int opcion = sc.nextInt();
+        do {
+            System.out.println("Ingrese:\n (0) salir \n (1) sumar \n (2) restar \n (3) multiplicar \n (4) dividir");
+            opcion = sc.nextInt();
+        }while(opcion < 0 || opcion > 4);
+
     }
 }
--- JavaG1/src/com/generation/f20220525/CondicionalSwitch.java
+++ JavaG1/src/com/generation/f20220525/CondicionalSwitch.java
@@ -7,22 +7,22 @@ public class CondicionalSwitch {
         Scanner sc = new Scanner(System.in);
         //menu
         System.out.println("Ingrese opcion:");
-        System.out.println(" 1 sumar \n 2 restar \n 3 multiplicar \n 4 dividir");
+        System.out.println("1 18 a 29 \n 2 30 - 49 \n 3 50 - 69 \n 4 más de 70\n 0 para salir");
         int opcion = sc.nextInt();
         //PEDIR LAS VARIABLES AQUI, FUERA DEL SWITCH
         switch (opcion) {
             case 1://sumar
                 //accion para opcion 1
-                System.out.println("** Suma");
+                System.out.println("100");
                 break;
             case 2:
-                System.out.println("** Resta");
+                System.out.println("250");
                 break;
             case 3:
-                System.out.println("** Multiplicacion");
+                System.out.println("400");
                 break;
             case 4:
-                System.out.println("** Division");
+                System.out.println("500");
                 break;
             default:
                 System.out.println("Opcion no valida");
